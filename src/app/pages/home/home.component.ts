7b7d83b70f58df4ea20e32b3321d9c63cd393fcb
Alan Ryan da Silva Domingues <alanryan619@gmail.com>
2025-07-12 20:58:39 -0300

    feat,style: adds animations and icons

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 
-import { TaskListComponent } from '../components/task-list/task-list.component';
+import { MatIconModule } from '@angular/material/icon';
 
+import { TaskListComponent } from '../components/task-list/task-list.component';
 import { TaskFormComponent } from '../components/task-form/task-form.component';
 
 @Component({
   selector: 'home',
-  imports: [TaskListComponent, TaskFormComponent],
+  imports: [TaskListComponent, TaskFormComponent,  MatIconModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
