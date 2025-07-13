583d5e9dac6f975f00c96df56593d8067f8939e9
Alan Ryan da Silva Domingues <alanryan619@gmail.com>
2025-07-12 21:44:21 -0300

    refactor(animations): migrate "page-footer" animation from CSS to "@angular/animations"

--- src/app/components/page-footer/page-footer.component.ts
+++ src/app/components/page-footer/page-footer.component.ts
@@ -1,11 +1,22 @@
+import { trigger, style, transition, animate } from '@angular/animations';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'page-footer',
+  standalone: true,
   imports: [],
   templateUrl: './page-footer.component.html',
-  styleUrl: './page-footer.component.css'
+  styleUrls: ['./page-footer.component.css'],
+  animations: [
+    trigger('slideUpAnimation', [
+      transition(':enter', [
+        style({ transform: 'translateY(100%)', opacity: 0 }),
+        animate(
+          '0.8s 1.2s ease-out',
+          style({ transform: 'translateY(0)', opacity: 1 })
+        ),
+      ]),
+    ]),
+  ],
 })
-export class PageFooterComponent {
-
-}
+export class PageFooterComponent {}
