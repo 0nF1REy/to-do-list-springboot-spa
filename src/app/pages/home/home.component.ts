218c3465d38ef940865d6237ba81f0a65a169649
Alan Ryan da Silva Domingues <alanryan619@gmail.com>
2025-07-12 21:58:39 -0300

    refactor(animations): migrate page "home" animation from CSS to "@angular/animations"

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,14 +1,46 @@
+import {
+  trigger,
+  transition,
+  style,
+  animate,
+  query,
+  stagger,
+} from '@angular/animations';
 import { Component } from '@angular/core';
-
 import { MatIconModule } from '@angular/material/icon';
-
 import { TaskListComponent } from '../components/task-list/task-list.component';
 import { TaskFormComponent } from '../components/task-form/task-form.component';
 
 @Component({
   selector: 'home',
-  imports: [TaskListComponent, TaskFormComponent,  MatIconModule],
+  standalone: true,
+  imports: [TaskListComponent, TaskFormComponent, MatIconModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css',
+  styleUrls: ['./home.component.css'],
+  animations: [
+    trigger('cascadeIn', [
+      transition(':enter', [
+        query(
+          '.vintage-title, .vintage-grid > section',
+          [style({ opacity: 0, transform: 'translateY(40px)' })],
+          { optional: true }
+        ),
+        query(
+          '.vintage-title, .vintage-grid > section',
+          stagger('300ms', [
+            animate(
+              '0.7s ease-out',
+              style({ opacity: 1, transform: 'translateY(0)' })
+            ),
+          ]),
+          { optional: true }
+        ),
+      ]),
+      transition(':leave', [animate('0.4s ease-in', style({ opacity: 0 }))]),
+    ]),
+  ],
+  host: {
+    '[@cascadeIn]': '',
+  },
 })
 export class HomeComponent {}
